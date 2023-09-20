73ab2b46870c5d7f6acb41b03c4b48651dd4ebf9
titorodrigues17 <118935495+titorodrigues17@users.noreply.github.com>
2023-09-20 13:50:00 -0500

    Add files via upload

---
+++ JavaPoo/src/main/java/org/uts/Doctor.java
@@ -0,0 +1,14 @@
+package org.uts;
+
+public class Doctor {
+    private int id;
+    public String name;
+    private String specialty;
+
+    //Comportamientos (Metodos)
+
+    //El void es por que no tendre algo de retorno
+    public void showName(){
+        System.out.println(name);
+    }
+}
--- JavaPoo/src/main/java/org/uts/Main.java
+++ JavaPoo/src/main/java/org/uts/Main.java
@@ -1,19 +1,19 @@
 package org.uts;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    /* Para nosotros comunicar de una clase a otra en este
+    caso de la clase Main a la clase Doctor entonces debemos
+     instanciar el objeto o en este caso la clase
+     Doctor doctor = new Doctor();
+    */
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    //Como instanciar un objeto( Nombre de la clase + nombre del objeto = new + nombre de la clase)
+    Doctor doctor = new Doctor();
+    doctor.name = "Carlos Santos";
+    doctor.showName();
+
+
     }
 }
